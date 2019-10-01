e44b2b6d75df2c8779689bfe393217a67268b9fe
Yeferal <yefercuber20@gmail.com>
2019-10-01 03:58:55 -0600

    Se crea el package json

--- verificacion.js
+++ verificacion.js
@@ -1,1 +1,16 @@
-alert("Hola por el momento solo es el inicio");
+// alert("Hola por el momento solo es el inicio");
+console.log("Hola por el momento solo es el inicio");
+
+function verificarEstoA(letra){
+
+    switch (letra) {
+        case "a":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
